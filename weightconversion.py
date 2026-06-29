7506d9e727014eb2f5c3bda5b004755855b2803c
Ritikkkkk!! <editzritik42@gmail.com>
2026-06-29 14:14:45 +0530

    Add files via upload

--- weightconversion.py
+++ weightconversion.py
@@ -3,13 +3,18 @@ print("Welcome to weight converter")
 print("what you want to do ?")
 print("choose 1 to convert lbs to kg \n choose 2 to convert kg to lbs")
 x = int(input("choose 1 or 2"))
-if x==1:
+
+if x == 1:
     print("LBS to KG")
-    lbs = int(input(" enter lbs ="))
-    print(lbs/2.2,"kg")
-elif x==2:
-    print("kg to LBS")
-    kg = int(input("enter kg ="))
-    print(kg * 2 / 10)
+    lbs = float(input(" enter lbs ="))
+    kg = lbs / 2.20462
+    print(kg, "kg")
+
+elif x == 2:
+    print("KG to LBS")
+    kg = float(input("enter kg ="))
+    lbs = kg * 2.20462
+    print(lbs, "lbs")
+
 else:
     print("choose right option")
