7a05e8042e015f1d1188f1ed4b0ce78f55312a6c
rxt1k <editzritik42@gmail.com>
2026-07-01 15:16:28 +0530

    new file

--- tempconversion.py
+++ tempconversion.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #temperature conversion
 unit = input("is this temp in celsius or farhrenheit (C/F) : ")
 temp = float(input("enter temperature = "))
@@ -9,16 +8,4 @@ elif unit=="F":
     temp = (temp -32) * 5 / 9 
     print("the temperature in celsius is",temp,"C")
 else:
-=======
-#temperature conversion
-unit = input("is this temp in celsius or farhrenheit (C/F) : ")
-temp = float(input("enter temperature = "))
-if unit=="C":
-    temp = round((9 * temp) / 5 + 32)
-    print("the temperature in farhrenheit is",temp,"F")
-elif unit=="F":
-    temp = (temp -32) * 5 / 9 
-    print("the temperature in celsius is",temp,"C")
-else:
->>>>>>> 6b8cefa2113b74ac278ceb4560e1434e8ae9f17f
     print("invalid unit")
--- weightconversion.py
+++ weightconversion.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # lbs to kg converter
 print("Welcome to weight converter")
 print("what you want to do ?")
@@ -18,25 +17,4 @@ elif x == 2:
     print(lbs, "lbs")
 
 else:
-=======
-# lbs to kg converter
-print("Welcome to weight converter")
-print("what you want to do ?")
-print("choose 1 to convert lbs to kg \n choose 2 to convert kg to lbs")
-x = int(input("choose 1 or 2"))
-
-if x == 1:
-    print("LBS to KG")
-    lbs = float(input(" enter lbs ="))
-    kg = lbs / 2.20462
-    print(kg, "kg")
-
-elif x == 2:
-    print("KG to LBS")
-    kg = float(input("enter kg ="))
-    lbs = kg * 2.20462
-    print(lbs, "lbs")
-
-else:
->>>>>>> 6b8cefa2113b74ac278ceb4560e1434e8ae9f17f
     print("choose right option")
