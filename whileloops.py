8d3a1b5c76dec520c96718c2fe6307307a2fb926
rxt1k <editzritik42@gmail.com>
2026-07-02 15:55:13 +0530

    table

--- whileloops.py
+++ whileloops.py
@@ -1,2 +1,5 @@
-#while loops = execute some code while some conditions are remains true
-
+table = int(input("enter the table you want : "))
+i = 0
+while i<10:
+    i = i + 1
+    print(f"{table} x {i} = ",table*i)
